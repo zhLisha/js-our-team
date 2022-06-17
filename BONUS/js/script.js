59f98accda7d7647a4e48563384d3060ab237534
zhLisha <lisazhou1709@gmail.com>
2022-06-17 14:29:38 +0200

    generate team cards with function done

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -41,4 +41,34 @@ const allTeams = [
 ];
 
 
+// Richiamo il mio elemento del DOM a cui dovro' inserire tutte le card del team
+const allTeamGrid = document.querySelector('.team-container');
 
+// Invoco la mia funzione per scorrere l'array
+allTeamObject(allTeams);
+ 
+// STAMPARE PER OGNI MEMBRO LA SUA CARD
+// Scorro tutta l'array per avere gli object al suo interno
+function allTeamObject(teamsInfo) {
+    for(let i = 0; i < teamsInfo.length; i++) {
+        const singleCard = teamsInfo[i]; 
+
+        // Invoco la mia funzione per tirare fuori tutti i valori di ogni object
+        singleObjectInfo(singleCard)
+    }
+}
+
+// Tiro fuori i valori dai singoli elementi dell'object e li inserisco nel nuovo DIV da inserire a team-container
+function singleObjectInfo(arrayTeamElement) {
+    const newDiv = `
+    <div class="team-card">
+        <div class="card-image">
+        <img src="img/${arrayTeamElement.image}" alt="Wayne Barnett"/>
+        </div>
+        <div class="card-text">
+        <h3>${arrayTeamElement.name}</h3>
+        <p>${arrayTeamElement.role}</p>
+        </div>`
+    // Inserisco newDiv alla classe team-container del DOM per creare le effettive CARD di ogni membro
+    allTeamGrid.innerHTML += newDiv;
+}
