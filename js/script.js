baad06de63582a7a6a9c5d0ae3ada2ddc3af9ba0
zhLisha <lisazhou1709@gmail.com>
2022-06-17 13:50:07 +0200

    Created array with all team info

--- js/script.js
+++ js/script.js
@@ -3,5 +3,40 @@
     a. Nome Cognome
     b. Ruolo
     c. Foto
-- Stampare tutte per ogni membro la sua card.
+- Stampare per ogni membro la sua card.
 */
+
+// Creare un array contentente tutti i dati del team fisso
+const allTeams = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg',
+    }
+];
+console.log('Array tutto il team', allTeams);
