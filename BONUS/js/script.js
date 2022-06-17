fa60cc4f9f1eb8223a7fbe322bda92e45257a618
zhLisha <lisazhou1709@gmail.com>
2022-06-17 14:17:05 +0200

    created bonus folder

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -41,25 +41,4 @@ const allTeams = [
 ];
 
 
-// Richiamo il mio elemento del DOM a cui dovro' inserire tutte le card del team
-const allTeamGrid = document.querySelector('.team-container');
-
-// STAMPARE PER OGNI MEMBRO LA SUA CARD
-// Scorro tutta l'array per avere gli object al suo interno
-for(let i = 0; i < allTeams.length; i++) {
-    const singleCard = allTeams[i];
-
-    // Tiro fuori i valori dai singoli elementi dell'object e li inserisco nel nuovo DIV da inserire a team-container
-    const newDiv = `
-    <div class="team-card">
-        <div class="card-image">
-        <img src="img/${singleCard.image}" alt="Wayne Barnett"/>
-        </div>
-        <div class="card-text">
-        <h3>${singleCard.name}</h3>
-        <p>${singleCard.role}</p>
-        </div>`
-    // Inserisco newDiv alla classe team-container del DOM per creare le effettive CARD di ogni membro
-    allTeamGrid.innerHTML += newDiv;
-}
 
